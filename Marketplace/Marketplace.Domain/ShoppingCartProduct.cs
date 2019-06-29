e410dd54f70ad89fc3422fe25119149a958ac6a6
GHorozov <georgihorozov@hotmail.com>
2019-06-29 16:23:48 +0300

    delete empty lines

--- Marketplace/Marketplace.Domain/ShoppingCartProduct.cs
+++ Marketplace/Marketplace.Domain/ShoppingCartProduct.cs
@@ -9,7 +9,6 @@ namespace Marketplace.Domain
         public string ShoppingCartId { get; set; }
         public virtual ShoppingCart ShoppingCart { get; set; }
 
-
         public string ProductId { get; set; }
         public virtual Product Product { get; set; }
     }
