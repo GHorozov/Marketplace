560072c962b2c0809dcad424746243c660a455d1
GHorozov <georgihorozov@hotmail.com>
2019-07-23 04:11:39 +0300

    Add ICategoryService interface

---
+++ Marketplace/Marketplace.Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Marketplace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marketplace.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        IQueryable<TModel> GetAllCategories<TModel>();
+
+        Category GetCategoryById(string id);
+
+        void Create(string name);
+
+        void Edit(string id, string name);
+    }
+}
