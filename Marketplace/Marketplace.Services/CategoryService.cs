d37b528ad9339e6fd1163c298bade6d0a66417e9
GHorozov <georgihorozov@hotmail.com>
2019-07-23 04:10:13 +0300

    Add CategoryService

---
+++ Marketplace/Marketplace.Services/CategoryService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Marketplace.Data;
+using Marketplace.Domain;
+using Marketplace.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marketplace.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly MarketplaceDbContext context;
+        private readonly IMapper mapper;
+
+        public CategoryService(MarketplaceDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IQueryable<TModel> GetAllCategories<TModel>()
+        {
+            var result = this.context
+                .Categories
+                .ProjectTo<TModel>(mapper.ConfigurationProvider);
+
+            return result;
+        }
+
+        public void Create(string name)
+        {
+            var category = new Category() { Name = name };
+
+            this.context.Categories.Add(category);
+            this.context.SaveChanges();
+        }
+
+        public Category GetCategoryById(string id)
+        {
+            var category = this.context
+                .Categories
+                .SingleOrDefault(x => x.Id == id);
+
+            return category;
+        }
+
+        public void Edit(string id, string name)
+        {
+            var category = this.GetCategoryById(id);
+            if (category == null)
+            {
+                return;
+            }
+
+            category.Name = name;
+
+            this.context.Categories.Update(category);
+            this.context.SaveChanges();
+        }
+    }
+}
