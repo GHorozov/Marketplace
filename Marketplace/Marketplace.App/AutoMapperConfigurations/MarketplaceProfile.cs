d9a255e6e57eadea366d7aeb2644ee14750ffee7
GHorozov <georgihorozov@hotmail.com>
2019-07-24 13:08:41 +0300

    Add new configurations

--- Marketplace/Marketplace.App/AutoMapperConfigurations/MarketplaceProfile.cs
+++ Marketplace/Marketplace.App/AutoMapperConfigurations/MarketplaceProfile.cs
@@ -3,6 +3,7 @@ using Marketplace.Domain;
 using Marketplace.App.Areas.Administrator.ViewModels.Users;
 using System.Linq;
 using Marketplace.App.Areas.Administrator.ViewModels.Categories;
+using Marketplace.App.ViewModels.Components;
 
 namespace Marketplace.App.AutoMapperConfigurations
 {
@@ -17,6 +18,11 @@ namespace Marketplace.App.AutoMapperConfigurations
 
             this.CreateMap<Category, CategoryViewModel>();
             this.CreateMap<Category, EditCategoryViewModel>();
+
+            this.CreateMap<Category, IndexCategoryViewModel>();
+            this.CreateMap<ShoppingCart, IndexShoppingCartViewModel>()
+                .ForMember(isc => isc.ProductCount, sc => sc.MapFrom(x => x.Products.Count()))
+                .ForMember(isc => isc.TotalPrice, sc => sc.MapFrom(x => x.Products.Sum(p => p.Product.Price)));
         }
     }
 }
