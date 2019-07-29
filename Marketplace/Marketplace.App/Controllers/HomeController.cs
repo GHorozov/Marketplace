91289519d6e19f1c23e0d841b5ffc83741a8f85b
GHorozov <georgihorozov@hotmail.com>
2019-07-29 12:42:37 +0300

    make changes to home controller

--- Marketplace/Marketplace.App/Controllers/HomeController.cs
+++ Marketplace/Marketplace.App/Controllers/HomeController.cs
@@ -5,16 +5,39 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Marketplace.App.Models;
 using Microsoft.AspNetCore.Authorization;
+using Marketplace.Services.Interfaces;
+using Marketplace.App.ViewModels.Home;
+using AutoMapper;
 
 namespace Marketplace.App.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMapper mapper;
+        private readonly ICategoryService categoryService;
+        private readonly IProductService productService;
+
+        public HomeController(IMapper mapper, ICategoryService categoryService, IProductService productService)
+        {
+            this.mapper = mapper;
+            this.categoryService = categoryService;
+            this.productService = productService;
+        }
+
         [AllowAnonymous]
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var categories = this.categoryService.GetAllCategories<HomeCategoryViewModel>().ToList();
+            var products = this.productService.GetAllProducts<HomeProductViewModel>().ToList();
+            
+            var resultModel = new HomeIndexViewModel()
+            {
+                 Categories = categories,
+                 Products = products
+            };
+
+            return this.View(resultModel);
         }
 
         public IActionResult Privacy()
