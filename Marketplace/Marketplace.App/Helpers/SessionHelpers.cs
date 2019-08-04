46fff49f17754015dab58d36febb7bb4a9e0a53f
GHorozov <georgihorozov@hotmail.com>
2019-08-04 12:37:46 +0300

    Add session helper

---
+++ Marketplace/Marketplace.App/Helpers/SessionHelpers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.Helpers
+{
+    public static class SessionHelpers
+    {
+        public static TModel GetObjectFromJson<TModel>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            var result = value == null ? default(TModel) : JsonConvert.DeserializeObject<TModel>(value);
+
+            return result;
+        }
+        
+        public static void SetObjectToJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value, new JsonSerializerSettings()));
+        }
+    }
+}
