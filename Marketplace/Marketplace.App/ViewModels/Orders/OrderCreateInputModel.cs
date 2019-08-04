6c085d7946f6aa3b7dfbaf8940ebc2e40f605ebd
GHorozov <georgihorozov@hotmail.com>
2019-08-04 12:39:36 +0300

    Add views for Orders

---
+++ Marketplace/Marketplace.App/ViewModels/Orders/OrderCreateInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marketplace.App.ViewModels.Orders
+{
+    public class OrderCreateInputModel
+    {
+        private const int AddressMinLenght = 10;
+        private const int AddressMaxLenght = 120;
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(AddressMaxLenght, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = AddressMinLenght)]
+        public string ShippingAddress { get; set; }
+    }
+}
