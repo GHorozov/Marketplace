20a5676bc34bb93591e1cba47c3cd6cfad965994
GHorozov <georgihorozov@hotmail.com>
2019-08-04 12:45:46 +0300

    Add migrations

---
+++ Marketplace/Marketplace.Data/Migrations/20190730135710_RemoveColorEntityAndRenamePictureProperty.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Marketplace.Data.Migrations
+{
+    public partial class RemoveColorEntityAndRenamePictureProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Colors_ColorId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Colors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ColorId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Pictures");
+
+            migrationBuilder.RenameColumn(
+                name: "ColorId",
+                table: "Products",
+                newName: "Color");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Color",
+                table: "Products",
+                newName: "ColorId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ColorId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Pictures",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Colors",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ProductId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Colors_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ColorId",
+                table: "Products",
+                column: "ColorId",
+                unique: true,
+                filter: "[ColorId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colors_ProductId",
+                table: "Colors",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Colors_ColorId",
+                table: "Products",
+                column: "ColorId",
+                principalTable: "Colors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
