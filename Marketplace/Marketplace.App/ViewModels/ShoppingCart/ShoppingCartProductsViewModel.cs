4d7efee26099272b51c48024c3e268b24766a3d7
GHorozov <georgihorozov@hotmail.com>
2019-08-04 12:40:43 +0300

    Add view model for ShoppingCart

---
+++ Marketplace/Marketplace.App/ViewModels/ShoppingCart/ShoppingCartProductsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.ViewModels.ShoppingCart
+{
+    public class ShoppingCartProductsViewModel
+    {
+        public List<ShoppingCartViewModel> Products { get; set; }
+
+        public decimal TotalProductsCost => this.Products.Select(x => x.Total).Sum();
+    }
+}
---
+++ Marketplace/Marketplace.App/ViewModels/ShoppingCart/ShoppingCartViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Marketplace.App.ViewModels.ShoppingCart
+{
+    public class ShoppingCartViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Color { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public decimal Total => this.Quantity * this.Price;
+    }
+}
