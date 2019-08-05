56e242bd52617f38a0f479bed9f34c1010aef33c
GHorozov <georgihorozov@hotmail.com>
2019-08-05 04:15:01 +0300

    Add To Categories/CategoriesProductViewModel

---
+++ Marketplace/Marketplace.App/ViewModels/Categories/CategoriesProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Marketplace.App.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.ViewModels.Categories
+{
+    public class CategoriesProductViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Picture { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public bool IsNew => DateTime.UtcNow.Subtract(this.PublishDate).Days < GlobalConstants.LessThanDaysIsNew;
+    }
+}
