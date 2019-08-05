8f2629d56c01ee687417720f56e6578c59615db9
GHorozov <georgihorozov@hotmail.com>
2019-08-05 04:12:55 +0300

    Add property IsNew in AllProductViewModel

--- Marketplace/Marketplace.App/ViewModels/Products/AllProductViewModel.cs
+++ Marketplace/Marketplace.App/ViewModels/Products/AllProductViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Marketplace.Domain;
+﻿using Marketplace.App.Infrastructure;
+using Marketplace.Domain;
+using System;
 using System.Collections.Generic;
 
 namespace Marketplace.App.ViewModels.Products
@@ -12,5 +14,9 @@ namespace Marketplace.App.ViewModels.Products
         public decimal Price { get; set; }
 
         public string Picture { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public bool IsNew => DateTime.UtcNow.Subtract(this.PublishDate).Days < GlobalConstants.LessThanDaysIsNew; 
     }
 }
