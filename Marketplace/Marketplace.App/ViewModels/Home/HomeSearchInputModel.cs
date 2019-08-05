fd7c4af326a64d8da2576ff007d8c25bcdd7abeb
GHorozov <georgihorozov@hotmail.com>
2019-08-05 04:17:01 +0300

    Add view models for home controller search action

---
+++ Marketplace/Marketplace.App/ViewModels/Home/HomeSearchInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.ViewModels.Home
+{
+    public class HomeSearchInputModel
+    {
+        public string Input { get; set; }
+
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Marketplace/Marketplace.App/ViewModels/Home/HomeSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Marketplace.App.Infrastructure;
+using System;
+
+namespace Marketplace.App.ViewModels.Home
+{
+    public class HomeSearchViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Picture { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public bool IsNew => DateTime.UtcNow.Subtract(this.PublishDate).Days < GlobalConstants.LessThanDaysIsNew;
+    }
+}
