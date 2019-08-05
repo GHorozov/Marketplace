9be021b0d551835fa5efb5c09b4e9022b1d314b7
GHorozov <georgihorozov@hotmail.com>
2019-08-05 04:14:01 +0300

    Add WishList/WishListProductViewModel

---
+++ Marketplace/Marketplace.App/ViewModels/WishList/WishListProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.ViewModels.WishList
+{
+    public class WishListProductViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Color { get; set; }
+
+        public string PictureUrl { get; set; }
+    }
+}
