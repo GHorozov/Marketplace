973d023c6702892347523849bc0f9517a5479b02
GHorozov <georgihorozov@hotmail.com>
2019-08-05 04:28:18 +0300

    Add SideBarViewComponent

---
+++ Marketplace/Marketplace.App/Components/SideBarViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Marketplace.App.ViewModels.Components;
+using Marketplace.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.Components
+{
+    public class SideBarViewComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+        public SideBarViewComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allCategories = this.categoryService.GetAllCategories<SideBarCategoryViewModel>().ToList();
+            var resultModel = new SideBarViewModel() { Categories = allCategories };
+
+            return this.View(resultModel);
+        }
+    }
+}
