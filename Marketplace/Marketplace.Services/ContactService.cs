1b93d761eed55318c8de0400c019f4b56351dd2a
GHorozov <georgihorozov@hotmail.com>
2019-08-06 16:04:35 +0300

    Add ConntactService

---
+++ Marketplace/Marketplace.Services/ContactService.cs
@@ -0,0 +1,14 @@
+﻿using Marketplace.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Marketplace.Services
+{
+    public class ContactService : IContactService
+    {
+        public Task<bool> Create(string name, string email, string phone, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
