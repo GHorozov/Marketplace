79a8ef62b098547b456703c79559cb0ffb193746
GHorozov <georgihorozov@hotmail.com>
2019-08-06 16:08:26 +0300

    Add new constants

--- Marketplace/Marketplace.App/Infrastructure/GlobalConstants.cs
+++ Marketplace/Marketplace.App/Infrastructure/GlobalConstants.cs
@@ -16,7 +16,14 @@
         public const int ProductDescriptionMinLenght = 30;
         public const int ProductDescriptionMaxLenght = 300;
 
+        public const int ContactUserNameMinLenght = 2;
+        public const int ContactUserNameMaxLenght = 50;
+        public const int ContactMessageMinLenght = 30;
+        public const int ContactMessageMaxLenght = 250;
+
         public const int LessThanDaysIsNew = 3;
+        public const string SearchCategoryDefaultValue = "All Categories";
+        public const string HeadTextForFoundResult = "Found results";
 
         public const string DefaultPicturesPath = "wwwroot/images/users/";
     }
