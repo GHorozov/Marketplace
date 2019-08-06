3e5e9ed5cb9a12e3324936cfe21d36ba0d0a9c06
GHorozov <georgihorozov@hotmail.com>
2019-08-06 16:10:48 +0300

    Add Contact/ContactInputModel

---
+++ Marketplace/Marketplace.App/ViewModels/Contact/ContactInputModel.cs
@@ -0,0 +1,24 @@
+﻿using Marketplace.App.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace Marketplace.App.ViewModels.Contact
+{
+    public class ContactInputModel
+    {
+        [Required]
+        [StringLength(GlobalConstants.ContactUserNameMaxLenght, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = GlobalConstants.ContactUserNameMinLenght)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(GlobalConstants.ContactMessageMaxLenght, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = GlobalConstants.ContactMessageMinLenght)]
+        public string Message { get; set; }
+    }
+}
