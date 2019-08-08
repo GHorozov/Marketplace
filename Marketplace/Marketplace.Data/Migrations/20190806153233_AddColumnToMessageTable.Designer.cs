9f9abf862f90b989b9ccc72a6c69eb93113a94ec
GHorozov <georgihorozov@hotmail.com>
2019-08-08 04:05:46 +0300

    Add migration in Messages for adding property MarkAsRead

--- Marketplace/Marketplace.Data/Migrations/20190806153233_AddColumnToMessageTable.Designer.cs
+++ Marketplace/Marketplace.Data/Migrations/20190806153233_AddColumnToMessageTable.Designer.cs
@@ -4,14 +4,16 @@ using Marketplace.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Marketplace.Data.Migrations
 {
     [DbContext(typeof(MarketplaceDbContext))]
-    partial class MarketplaceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190806153233_AddColumnToMessageTable")]
+    partial class AddColumnToMessageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Marketplace/Marketplace.Data/Migrations/20190806153233_AddColumnToMessageTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Marketplace.Data.Migrations
+{
+    public partial class AddColumnToMessageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MarkAsRead",
+                table: "Messages",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarkAsRead",
+                table: "Messages");
+        }
+    }
+}
