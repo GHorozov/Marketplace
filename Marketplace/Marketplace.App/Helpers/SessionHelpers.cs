72317bf9a19019c61b764442e61d83f30735658e
GHorozov <georgihorozov@hotmail.com>
2019-08-10 19:53:27 +0300

    Add change to session helpers

--- Marketplace/Marketplace.App/Helpers/SessionHelpers.cs
+++ Marketplace/Marketplace.App/Helpers/SessionHelpers.cs
@@ -12,14 +12,21 @@ namespace Marketplace.App.Helpers
         public static TModel GetObjectFromJson<TModel>(this ISession session, string key)
         {
             var value = session.GetString(key);
-            var result = value == null ? default(TModel) : JsonConvert.DeserializeObject<TModel>(value);
+            return value == null ? default(TModel) : JsonConvert.DeserializeObject<TModel>(value);
 
-            return result;
+            //var value = session.GetString(key);
+            //var result = value == null ? default(TModel) : JsonConvert.DeserializeObject<TModel>(value);
+
+            //return result;
         }
         
         public static void SetObjectToJson(this ISession session, string key, object value)
         {
-            session.SetString(key, JsonConvert.SerializeObject(value, new JsonSerializerSettings()));
+            session.SetString(key, JsonConvert.SerializeObject(value, new JsonSerializerSettings
+            {
+            }));
+
+            // session.SetString(key, JsonConvert.SerializeObject(value, new JsonSerializerSettings() { }));
         }
     }
 }
