07625c5f13a987de6f72821475a144858bd50215
GHorozov <georgihorozov@hotmail.com>
2019-08-10 23:44:49 +0300

    add view models for orders

---
+++ Marketplace/Marketplace.App/ViewModels/Orders/MyOrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Marketplace.App.ViewModels.Orders
+{
+    public class MyOrderViewModel
+    {
+        public string Id { get; set; }
+
+        public DateTime IssuedOn { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ Marketplace/Marketplace.App/ViewModels/Orders/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.ViewModels.Orders
+{
+    public class OrderViewModel
+    {
+        public string Id { get; set; }
+
+        public DateTime IssuedOn { get; set; }
+
+        public int ProductsCount { get; set; }
+
+        public decimal OrderTotal { get; set; }
+    }
+}
